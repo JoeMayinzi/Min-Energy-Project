9847e344d8f559c8d34bacc6e0907d0e473da1f8
Joe Mayinzi <joel.mayinzi@groukam.com>
2022-06-27 19:01:33 +0200

    integration btn exit sidebar

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
 const sidebarBtn = document.querySelector(".sidebar-button i");
 const sidebarMenu = document.querySelector(".sidebar-menu");
+const sidebarExit = document.querySelector(".exit i");
+console.log(sidebarExit)
 
-sidebarBtn.addEventListener("click", (e)=> {
-    sidebarMenu.style.left = 0
+sidebarBtn.addEventListener("click", ()=> {
+    sidebarMenu.style.right = 0;
+    
+})
+
+sidebarExit.addEventListener("click", ()=>{
+    sidebarMenu.style.right = -450 + "px";
 })
