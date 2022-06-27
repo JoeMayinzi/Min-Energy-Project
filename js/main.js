7148ef91d6af0257f3562fa5cfba2d3441a8fc4f
Joe Mayinzi <joel.mayinzi@groukam.com>
2022-06-27 18:39:28 +0200

    integration du commit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
-console.log("menu humnerger")
+const sidebarBtn = document.querySelector(".sidebar-button i");
+const sidebarMenu = document.querySelector(".sidebar-menu");
+
+sidebarBtn.addEventListener("click", (e)=> {
+    sidebarMenu.style.left = 0
+})
