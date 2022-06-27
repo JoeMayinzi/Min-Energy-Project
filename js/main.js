ba5b7049af21c175baa5ae75486aa478c0bed3fe
Joe Mayinzi <joel.mayinzi@groukam.com>
2022-06-27 23:10:27 +0200

    liaison des pages

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 const sidebarBtn = document.querySelector(".sidebar-button i");
 const sidebarMenu = document.querySelector(".sidebar-menu");
 const sidebarExit = document.querySelector(".exit i");
+const links = document.querySelectorAll(".navbar-nav a");
+const activePage = window.location.pathname
 
-const hideSidebar =
 
 sidebarBtn.addEventListener("click", ()=> {
     sidebarMenu.style.right = 0;
@@ -12,5 +13,12 @@ sidebarBtn.addEventListener("click", ()=> {
 sidebarExit.addEventListener("click", (e)=>{
     e.preventDefault()
     sidebarMenu.style.right = -450 + "px";
-    /*sidebarMenu.style.visibility = "hidden"*/
+    sidebarMenu.style.visibility = "hidden"
+})
+
+links.forEach((link) => {
+    if (link.href.includes(`${activePage}`)) {
+        link.classList.add("activeLink");
+        console.log(link)
+    }
 })
