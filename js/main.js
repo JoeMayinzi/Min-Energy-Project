d25009052ad55bcac0e4f0c35ae05c0f403ae896
Joe Mayinzi <joel.mayinzi@groukam.com>
2022-07-08 21:55:56 +0200

    Change project

--- js/main.js
+++ js/main.js
@@ -1,9 +1,1 @@
-/*const links = document.querySelectorAll(".navbar-nav a");*/
-/*const activePage = window.location.pathname;
-const headerNav = document.querySelector("header nav");
-const searchBtn = document.querySelector(".search-btn i");
-const searchInput = document.querySelector(".search-input input");
 
-searchBtn.addEventListener("click", ()=>{
-    searchInput.classList.toggle("active")
-})*/
