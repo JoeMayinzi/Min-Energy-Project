00ff11aa557e55f035f697a4c6b121be15e5e95f
Joe Mayinzi <joel.mayinzi@groukam.com>
2022-07-11 23:19:46 +0200

    fixing the nexs-bar

--- js/main.js
+++ js/main.js
@@ -1,11 +1,25 @@
-const activePage = window.location.pathname
+const activePage = window.location.pathname;
 const navLinks = document.querySelectorAll("nav a");
+const searchIcon = document.querySelector(".search-box a");
+const header = document.querySelector("header");
+const body = document.querySelector("body")
+const modalFade = document.querySelector(".modal-backdrop.show");
+
 
 navLinks.forEach(link => {
     
     link.addEventListener("click", ()=>{
         if(link.href.includes(`${activePage}`)) {
-            link.classList.add("activeLink")
+            link.classList.add("activeLink");
         }
     })
 })
+
+/*searchIcon.addEventListener("click", (e)=>{
+    e.preventDefault()
+    console.log("cliqué");
+    header.classList.remove("sticky-top");
+    
+})*/
+
+
