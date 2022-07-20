f3be7d251e55f0a7441e6224e7f690394c333134
Joe Mayinzi <joel.mayinzi@groukam.com>
2022-07-20 18:56:58 +0200

    Retry active

--- js/main.js
+++ js/main.js
@@ -43,12 +43,14 @@ function backToTop() {
 }
 
 navLinks.forEach(link =>{
- if (link.href.includes(`${activePage}`)) {
-    link.classList.add("activeLink");
-    if (activePage == '/'){
-      link.classList.remove("activeLink")
-    }
-  } 
+  if (activePage == '/'){
+    link.classList.remove("activeLink");
+    if (link.href.includes(`${activePage}`)) {
+      link.classList.add("activeLink");
+     
+    } 
+  }
+ 
 
  
 })
