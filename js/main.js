50fc03152de33737d5a1f51e98e681958088f0c2
Joe Mayinzi <joel.mayinzi@groukam.com>
2022-07-20 16:20:41 +0200

    Derbières modifications

--- js/main.js
+++ js/main.js
@@ -5,6 +5,9 @@ const header = document.querySelector("header");
 const body = document.querySelector("body")
 const modalFade = document.querySelector(".modal-backdrop.show");
 
+//Get the button
+let mybutton = document.getElementById("btn-back-to-top");
+
 
 navLinks.forEach(link => {
     
@@ -15,11 +18,27 @@ navLinks.forEach(link => {
     })
 })
 
-/*searchIcon.addEventListener("click", (e)=>{
-    e.preventDefault()
-    console.log("cliqué");
-    header.classList.remove("sticky-top");
-    
-})*/
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
 
 
